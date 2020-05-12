4c845ddd7bb811e5a18633e7230add0510d11ece
nicholasstano <nicholasstano@gmail.com>
2020-05-12 13:33:29 -0400

    Putting Kyle's wireframes to life

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Containers/Navbar/Navbar.jsx'
+import Observe from './Containers/Observe/Observe';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Observe />
     </div>
   );
 }
--- src/Containers/Observe/Observe.jsx
+++ src/Containers/Observe/Observe.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import './observe.scss'
 
 const Observe = () => {
     return (
-        <div>
-
+        <div className="observe">
+            <div className="observeHeader">
+                <p>Teacher: Smith</p>
+                <p>Coach: Coapman</p>
+            </div>
+            <div className="observeTable">
+                <thead>
+                    <tr>
+                        <th>GBF Move:</th>
+                        <th>Score:</th>
+                        <th>Notes:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Routines and Procedures 101</td>
+                        <td>3</td>
+                        <td><input></input></td>
+                    </tr>
+                    <tr>
+                        <td>Strong Voice</td>
+                        <td>3</td>
+                        <td><input></input></td>
+                    </tr>
+                    <tr>
+                        <td>What to Do</td>
+                        <td>4</td>
+                        <td><input></input></td>
+                    </tr>
+                </tbody>
+            </div>
+            <div>
+                <p>Lastest Action Steps:</p>
+            </div>
         </div>
     )
 }

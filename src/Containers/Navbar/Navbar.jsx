fea712d9ca3b47cfb5c739b6f50acb534ff1e14f
nicholasstano <nicholasstano@gmail.com>
2020-05-12 13:16:17 -0400

    Started Navbar

---
+++ src/Containers/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <p>Get Better Faster 2.0 |</p>
+            <button>Observe</button>
+            <button>Feedback</button>
+            <button>Learn</button>
+            <button>Growth</button>
+            <button>Lead</button>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/Containers/Observe/Observe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Observe = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Observe

e60e5a9388e42b81c4028af216f2580f08822e6d
nicholasstano <nicholasstano@gmail.com>
2020-07-15 14:49:16 -0400

    Did work to start Bill's wireframe

--- src/Containers/ActionStep/ActionStep.jsx
+++ src/Containers/ActionStep/ActionStep.jsx
@@ -1,49 +1,55 @@
 import React, { Component } from 'react'
-import { databaseUrl } from '../../config'
 
 export class ActionStep extends Component {
-    _isMounted = false;
 
     state = {
-        openActionSteps: [],
-        moveName: []
+        closeFeedback: true,
+        closeLearn: true
     }
 
-    componentDidMount() {
-        this._isMounted = true;
-        this.fetchActionSteps()
+    toggleCloseFeedback = () => {
+        this.setState({ closeFeedback: !this.state.closeFeedback })
     }
 
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
-
-    fetchActionSteps = () => {
-        fetch(`${databaseUrl}/action_steps`)
-            .then(res => res.json())
-            .then(actionSteps => {
-                if (this._isMounted) {
-                    this.setState({
-                        openActionSteps: actionSteps.filter(actionStep =>
-                            !actionStep.is_completed
-                            // && actionStep.teacherId === this.props.user.id
-                        )
-                    })
-                }
-            })
+    toggleLearnFeedback = () => {
+        this.setState({ closeLearn: !this.state.closeLearn })
     }
 
     render() {
-        let openActionSteps = this.state.openActionSteps
-        let userActionSteps = openActionSteps.map(actionStep => <div key={actionStep.id}>
-            <p>{actionStep.long_text}</p>
-            <p>Date Assigned: {actionStep.date.slice(0, 10)}</p>
-            <hr />
-        </div>
+        let openTeacherFeedback = this.props.user.feedback.filter(feedback => !feedback.is_assigned)
+        let currentActionSteps = openTeacherFeedback.map(fb =>
+            <div key={fb.id}>
+                <p>{fb.action_step.long_text}</p>
+                <p>Date Assigned: {fb.action_step.date.slice(0, 10)}</p>
+                <p>Skill: {fb.move.name.split(".")[1]}</p>
+                <p>Grade: {fb.observation.score}</p>
+                <p onClick={this.toggleLearnFeedback}> {this.state.closeLearn ? <>+</> : <>-</>} Learn</p>
+                {this.state.closeLearn
+                    ?
+                    null
+                    :
+                    <div>
+                        <p>Notes: {fb.observation.notes}</p>
+                        <p>Comments: {fb.observation.comments}</p>
+                    </div>
+                }
+                <p onClick={this.toggleCloseFeedback}> {this.state.closeFeedback ? <>+</> : <>-</>} Feedback</p>
+                {this.state.closeFeedback
+                    ?
+                    null
+                    :
+                    <div>
+                        <p>Praise: {fb.praise}</p>
+                        <p>See It: {fb.see_it}</p>
+                        <p>Name It: {fb.name_it}</p>
+                        <p>Do It: {fb.do_it}</p>
+                        <p>Quick Hit: {fb.quick_hits}</p>
+                    </div>}
+            </div>
         )
         return (
             <div>
-                {userActionSteps}
+                {currentActionSteps}
             </div>
         )
     }
